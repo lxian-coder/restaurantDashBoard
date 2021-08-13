ecca63ea014975cc27067743767258c551bbe138
darcyxian <darcyxianau@gmail.com>
2021-08-13 23:48:20 +1000

    added passwordhint

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -3,6 +3,7 @@ import styled,{css} from 'styled-components';
 import CSSCONST from '../../cssConst';
 import { Switch, Route, withRouter, RouteComponentProps} from 'react-router-dom';
 import axios from 'axios';
+import { timers } from 'jquery';
 
 
 const LoginWarper = styled.div`
@@ -66,6 +67,7 @@ const Buttonwarper = styled.div`
 
  };
  interface State {
+     passwordHint:string,
  }
 
 class Login extends React.Component<Props,State>{
@@ -73,10 +75,39 @@ class Login extends React.Component<Props,State>{
     constructor(props:any){
         super(props);
         this.loginSubmit = this.loginSubmit.bind(this);
+        this.getPasswordHint = this.getPasswordHint.bind(this);
+        this.state={
+            passwordHint:null,
+        }
     }
+    async getPasswordHint(e: any) {
+        e.preventDefault();
+     
+       const username:string =(document.getElementById("username") as HTMLInputElement).value;
+       console.log("aaaa");
+       console.log(username);
+       console.log(CSSCONST.BACK_URL+"user/"+username);
+      
+        await axios({
+          method: "get",
+          url: CSSCONST.BACK_URL + "user/"+ username,
+        }).then(
+          (res) => {
+            const userGetDTO = res.data;
+            const passwordHint = userGetDTO.passwordHint;
+            console.log(userGetDTO.passwordHint);
+            if(passwordHint !== null)
+            this.setState({
+                passwordHint:passwordHint,
+            })
+
+          },
+          (error) => console.log(error)
+        );
+      }
+
     async loginSubmit(e: any) {
         e.preventDefault();
-        
         const fd = new FormData(e.target);
 
         const body = {
@@ -111,11 +142,12 @@ class Login extends React.Component<Props,State>{
         }}>
                    <Text>Login</Text>
                    <Label htmlFor='username'>User Name: </Label>
-                   <Input type='text' name='username' id='username' ></Input>
+                   <Input type='text' required name='username' id='username' ></Input>
                    <Label htmlFor='password'>Password:</Label>
-                   <Input type='text' name='password' id='password'></Input>
+                   <Input type='password' required name='password' id='password'></Input>
                    <Buttonwarper>
-                   <Button>Password hint</Button>
+                   <Button id="getName" onClick={(e)=>this.getPasswordHint(e)}>Password hint</Button>
+                   <div id="passwordHint">{this.state.passwordHint}</div>
                    <InputBtn type='submit'></InputBtn>
                    </Buttonwarper>
               </Form>
